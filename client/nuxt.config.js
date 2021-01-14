09aa4f70aa30cc031880e08295f34663e8cbc377
cnrd <hello@conradweise.com>
2021-01-14 14:56:16 +0100

    meta:image

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -11,7 +11,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Matters of Politics in Design Education, Research, Practice. The design conference ATTENDING [TO] FUTURES will take place at Köln International School of Design of TH Köln (Cologne, Germany) on 18-20 November 2021.' }
+      { hid: 'title', name: 'title', content: 'ATTENDING [TO] FUTURES' },
+      { hid: 'description', name: 'description', content: 'Matters of Politics in Design Education, Research, Practice. The design conference ATTENDING [TO] FUTURES will take place at Köln International School of Design of TH Köln (Cologne, Germany) on 18-20 November 2021.' },
+      { hid: 'image', name: 'image', content: 'https://www.attendingtofutures.de/attending-to-futures-meta.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
