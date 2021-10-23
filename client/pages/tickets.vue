89e569ddbf9d30c0742b08582b7308c2f1c940ec
Leon <leonfvogler@gmail.com>
2021-10-23 09:18:13 +0200

    add ticket area

--- client/pages/tickets.vue
+++ client/pages/tickets.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="page page-tickets">
     <div class="indent">
-      <p>Attending the conference as a listener will be free of charge. More information on how to attend (in person or online) will follow soon.</p>
+      <h2>Welcome to the ATTENDING [TO] FUTURES online ticket area!</h2>
+      <p>Attending the conference as a listener will be free of charge.</p>
+      <p>Please register for the <strong>General Admission</strong> ticket to access <strong>all keynotes, talks, and the roundtable, lecture performance, and theatre performance.</strong></p>
+      <p>If you would like to join one of the <strong>online workshops</strong>, register for a <strong>General Admission + Workshop</strong> ticket. The amount of tickets for each workshop is limited, please only register if you are absolutely certain you can attend. Keep in mind that you can only purchase one ticket per person.</p>
+      <p>If you see a workshop in the program that it not on the ticket list, it is because this workshop is happening on site only, and will not be accessible via stream.</p>
+      <p>
+        <button class="inline"><a href="https://hopin.com/events/attending-to-futures-matters-of-politics-in-design-education-research-practice" target="_bkank">Get your ticket</a></button></p>
+      <p>Each day you can follow one of the tracks (1) Contested Histories, (2) Radical Futures, or (3) Critical Practices, or switch between tracks. We are looking forward to sharing inspiring conversations and critical arguments with you!</p>
+      <p>Registration will close on November 17, 2021 / 06:30pm CET.</p>
+      <p>*Because of Covid-19 regulations, the on site participation will be restricted to KISD students, staff, and speakers. Information for KISD students on how to join will follow soon.</p>
     </div>
   </div>
 </template>
@@ -23,5 +32,8 @@ export default {
 <style scoped>
 .indent {
   max-width: 600px;
+}
+p {
+  margin: 1em 0;
 }
 </style>
